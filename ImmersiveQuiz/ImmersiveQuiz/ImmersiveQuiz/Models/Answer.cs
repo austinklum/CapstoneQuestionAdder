0e4294083dc6ce938ed0d3b2aee0e5070f364518
Austin Klum <klumaustin@gmail.com>
2021-04-26 03:03:25 -0500

    Answer cleanup

--- ImmersiveQuiz/ImmersiveQuiz/ImmersiveQuiz/Models/Answer.cs
+++ ImmersiveQuiz/ImmersiveQuiz/ImmersiveQuiz/Models/Answer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +11,15 @@ namespace ImmersiveQuiz.Models
     {
         public int AnswerId { get; set; }
 
+        [DisplayName("Answer")]
         public string Content { get; set; }
 
         public int QuestionId { get; set; }
+
+        [DisplayName("Correct Answer")]
+        public bool IsCorrect { get; set; }
+
+        [NotMapped]
+        public Question Question { get; set; }
     }
 }
