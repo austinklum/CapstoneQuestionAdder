9b44391e37fbe3821acf0cb419cfdde13d979edf
Austin Klum <klumaustin@gmail.com>
2021-07-11 19:55:35 -0500

    Added API endpoint to match PHP queries from before

---
+++ ImmersiveQuiz/ImmersiveQuiz/ImmersiveQuiz/Models/AnswerVR.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImmersiveQuiz.Models
+{
+    public class AnswerVR
+    {
+        public int AnswerId { get; set; }
+        public int QuestionId { get; set; }
+        public string Content { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
