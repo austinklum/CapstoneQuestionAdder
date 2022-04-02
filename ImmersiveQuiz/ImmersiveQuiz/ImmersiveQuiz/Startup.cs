252b02aebdc2dbdb653929c903895ce141d1f7e7
Austin Klum <klumaustin@gmail.com>
2022-04-02 16:14:23 -0500

    Created Login, Register, and Logout of users

--- ImmersiveQuiz/ImmersiveQuiz/ImmersiveQuiz/Startup.cs
+++ ImmersiveQuiz/ImmersiveQuiz/ImmersiveQuiz/Startup.cs
@@ -42,6 +42,8 @@ namespace ImmersiveQuiz
 
             services.AddDbContext<ScoreContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("ScoreContext")));
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +64,7 @@ namespace ImmersiveQuiz
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -69,6 +72,7 @@ namespace ImmersiveQuiz
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
